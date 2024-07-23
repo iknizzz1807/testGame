c5c47508e3356c817e97dfd8d07980f6a682d084
kohrabi <63339885+kohrabi@users.noreply.github.com>
2024-07-23 20:44:18 +0700

    Smooth movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,8 +6,6 @@ func _ready():
 	pass
 	
 
-
-
 @export var speed = 400
 
 func get_input():
@@ -27,7 +25,7 @@ func bulletOut():
 	get_parent().add_child(bullet);
 	bullet.position = position;
 	bullet.look_at(get_global_mouse_position());
-	bullet.get_child(0).direction = get_global_mouse_position() - bullet.position;
+	bullet.direction = get_global_mouse_position() - bullet.position;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
